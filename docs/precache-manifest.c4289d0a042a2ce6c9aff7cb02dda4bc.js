7027ac5e40308856170af34c5a4c258c1fe36ee8
Samwise <samgarg86@gmail.com>
2019-05-01 01:09:58 +0100

    homepage ./

---
+++ docs/precache-manifest.c4289d0a042a2ce6c9aff7cb02dda4bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dbc8610af2df16697934ab1771131f5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "24d7d966f4cf0b2c790e",
+    "url": "./static/css/main.d5b54138.chunk.css"
+  },
+  {
+    "revision": "65153ce114b7abe96479",
+    "url": "./static/js/2.74192dd0.chunk.js"
+  },
+  {
+    "revision": "24d7d966f4cf0b2c790e",
+    "url": "./static/js/main.032579b4.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "4c1bf61a6c12175b41945512e729b701",
+    "url": "./static/media/archive.4c1bf61a.svg"
+  },
+  {
+    "revision": "14945043e2b9fb099c463e54129593ea",
+    "url": "./static/media/delete.14945043.svg"
+  },
+  {
+    "revision": "a2fddfc17105b64a493f3b9c00394dca",
+    "url": "./static/media/edit.a2fddfc1.svg"
+  }
+]);
